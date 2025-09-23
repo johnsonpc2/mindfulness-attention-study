949df91247559240390347dfb89d18fd148f432c
Pierce Johnson <pjohnson4@albany.edu>
2025-09-23 15:27:38 +0000

    Update experiment.js

--- experiment.js
+++ experiment.js
@@ -1,1 +1,92 @@
+// First, we need to initialize jsPsych to run with Pavlovia
+const pavlovia_init = {
+  type: jsPsychPavlovia,
+  command: "init"
+};
 
+
+const pavlovia_finish = {
+  type: jsPsychPavlovia,
+  command: "finish"
+};
+
+const jsPsych = initJsPsych({
+});
+
+// Demographics survey: a block for entering age, gender, and race
+var demographics_age = {
+  type: jsPsychSurveyText,
+  questions: [{
+    prompt: '<p style=font-size:1.5vw>Please enter your age in numerals (e.g., "24")</p>',
+    name: 'age',
+    required: false
+  }],
+  data: {
+    phase: 'demographics_survey'
+  },
+  on_finish: function(data){
+                data.response = JSON.stringify(data.response.age);
+            }};
+
+var demographics_gender = {
+  type: jsPsychSurveyMultiSelect,
+  questions: [{
+    prompt: '<p style=font-size:1.5vw>Which of the following gender identities best describes you? Please select all that apply.</p>',
+    name: 'gender',
+    options: [
+      "Woman",
+      "Man",
+      "Transgender Woman",
+      "Transgender Man",
+      "Non-binary/gender non-conforming",
+      "Other",
+      "Prefer not to say"],
+    required: false,
+    vertical: true
+  }],
+  data: {
+    phase: 'demographics_gender'
+  }};
+
+var demographics_gender_other = {
+  type: jsPsychSurveyText,
+  questions: [{
+    prompt: '<p style=font-size:1.5vw>If you selected "Other", please specify. If you chose another option please answer "N/A"</p>',
+    name: 'gender_other',
+    required: false,
+    vertical: true
+  }],
+  data: {
+    phase: 'demographics_gender_other'
+  }};
+
+var demographics_race = {
+  type: jsPsychSurveyMultiChoice,
+  questions: [{
+    prompt: '<p style=font-size:1.5vw>Which of the following best describes you?</p>',
+    name: 'race',
+    options: [
+      "Asian or Pacific Islander",
+      "Black or African American",
+      "Hispanic or Latino",
+      "Indigenous or Native American",
+      "White or Caucasian",
+      "Multiracial"],
+    required: false,
+    vertical: true
+  }],
+  data: {
+    phase: 'demographics_race'
+  }};
+
+timeline.push(demographics_age);
+timeline.push(demographics_gender);
+timeline.push(demographics_gender_other);
+timeline.push(demographics_race);
+
+timeline.push(pavlovia_finish)
+
+// Runs the timeline we created with all the code we've put on it
+jsPsych.run(timeline);
+
+console.log(timeline);
